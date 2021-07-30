969c21da8da133b61c739fc2d9a9791dc6977501
Sanglim00 <sanglim@kookmin.ac.kr>
2021-07-30 20:12:09 +0900

    ~#3

--- myfirstapp/urls.py
+++ myfirstapp/urls.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+
+app_name = 'myfirstapp'
 urlpatterns = [
+    # ex: /myfirstapp/
     path('', views.index, name = 'index'),
+    # ex: /myfirstapp/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /myfirstapp/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: myfirstapp/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- myfirstapp/views.py
+++ myfirstapp/views.py
@@ -1,6 +1,25 @@
+from django import template
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, response
+from .models import Question
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('myfirstapp/index.html')
+    context = {'latest_question_list' : latest_question_list }
+    return render(request, 'myfirstapp/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'myfirstapp/detail.html', {'question' : question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." %question_id)
